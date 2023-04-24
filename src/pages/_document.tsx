61b07e1a068dfc86740521c5a75e1f59ffa066d4
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2023-04-23 23:35:02 -0500

    moved google fonts to cdn

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,11 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Sacramento&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Oooh+Baby&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Oooh+Baby&family=Raleway:wght@100;300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
